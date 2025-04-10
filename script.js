244bf081f13dc8c6e4d331bd899c1d20ad9addee
Mannubairos <manumanu80142@gmail.com>
2025-04-10 20:56:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function generateImage() {
+    const output = document.getElementById("output");
+    output.innerHTML = "<p>This is a demo. AI Ghibli style generation will be added soon!</p>";
+}
